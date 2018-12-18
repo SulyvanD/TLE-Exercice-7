007c75f9a32adfd6ac1a4a0bdbf5cf1fc81ce455
SulyvanD <sulyvan.debraize@net-c.com>
2018-12-18 09:55:28 +0100

    Exercice completé

---
+++ TLE - Exercice 7/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TLE___Exercice_7
+{
+    class Program
+    {
+        enum Week
+        {
+            Lundi,
+            Mardi,
+            Mercredi,
+            Jeudi,
+            Vendredi,
+            Samedi,
+            Dimanche
+        }
+
+        static void Main(string[] args)
+        {
+            Enum.GetValues(typeof(Week));
+            foreach (Week day in Enum.GetValues(typeof(Week)))
+            {
+                Console.WriteLine(day.ToString());
+            }
+            Console.WriteLine(Enum.GetValues(typeof(Week)).GetValue(4));
+            Console.ReadKey();
+        }
+    }
+}
