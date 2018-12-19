9738558cc6cf227e73e81dc6403833cb4ab467e2
Sulyvan Debraize <sulyvan.debraize@net-c.com>
2018-12-19 09:59:48 +0100

    Correction (Ajout de méthodes alternatives)

--- TLE - Exercice 7/Program.cs
+++ TLE - Exercice 7/Program.cs
@@ -21,13 +21,26 @@ namespace TLE___Exercice_7
 
         static void Main(string[] args)
         {
-            Enum.GetValues(typeof(Week));
+            // Affichage par boucle
             foreach (Week day in Enum.GetValues(typeof(Week)))
             {
                 Console.WriteLine(day.ToString());
             }
-            Console.WriteLine(Enum.GetValues(typeof(Week)).GetValue(4));
-            Console.ReadKey();
+            Console.WriteLine(Enum.GetNames(typeof(Week)).GetValue(4));
+
+            // Affichage à la main
+            Console.WriteLine(Week.Lundi);
+            Console.WriteLine(Week.Mardi);
+            Console.WriteLine(Week.Mercredi);
+            Console.WriteLine(Week.Jeudi);
+            Console.WriteLine(Week.Vendredi);
+            Console.WriteLine(Week.Samedi);
+            Console.WriteLine(Week.Dimanche);
+
+            // Différentes techniques pour afficher l'index 4
+            Console.WriteLine(Enum.GetNames(typeof(Week)).GetValue(4));
+            Console.WriteLine(Enum.GetName(typeof(Week), 4));
+            Console.WriteLine((Week)4);
         }
     }
 }
